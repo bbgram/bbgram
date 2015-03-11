d982eab89ae63e98f19fbea35d5127355481f5c1
moadib <moadib73rus@gmail.com>
2015-03-11 11:19:46 +0300

    Убран неиспользуемый код из main.cpp

--- bbgram/src/main.cpp
+++ bbgram/src/main.cpp
@@ -15,27 +15,8 @@ Q_DECL_EXPORT int main(int argc, char **argv)
 
     bb::cascades::Application app(argc, argv);
 
-    //QThread *thread = new QThread;
-    /*Telegraph* telegraph = new Telegraph;
-    telegraph->moveToThread(thread);
-    QObject::connect(thread, SIGNAL(started()), telegraph, SLOT(start()));
-    QObject::connect(telegraph, SIGNAL(finished()), thread, SLOT(quit()) );
-
-    QObject::connect(thread, SIGNAL(finished()), telegraph, SLOT(deleteLater()));
-    QObject::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
-
-    thread->start();*/
-
     ApplicationUI appui(&app);
     int result = bb::cascades::Application::exec();
-
-    /*telegraph->stop();
-    QMetaObject::invokeMethod(telegraph, "stop", Qt::QueuedConnection);
-    if(!thread->wait(1000))
-    {
-        thread->terminate();
-        thread->wait();
-    }*/
 
     return result;
 }
