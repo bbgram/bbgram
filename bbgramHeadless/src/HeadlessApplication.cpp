9798b11c92099a78fdfc1f8ab7ade908c940e3e3
Sergey Vikhirev <truesmb@gmail.com>
2015-03-24 10:41:06 +0300

    Работа с PushPayload

--- bbgramHeadless/src/HeadlessApplication.cpp
+++ bbgramHeadless/src/HeadlessApplication.cpp
@@ -1,5 +1,7 @@
 #include "HeadlessApplication.hpp"
 #include <bb/network/PushErrorCode>
+#include <bb/network/PushPayload>
+#include <bb/data/JsonDataAccess>
 #include "UDSWrapper.h"
 
 //http://pushapi.eval.blackberry.com
@@ -10,7 +12,7 @@ HeadlessApplication::HeadlessApplication(bb::Application *app) :
 
     QObject::connect(m_invokeManager, SIGNAL(invoked(const bb::system::InvokeRequest&)), this, SLOT(onInvoked(const bb::system::InvokeRequest&)));
 
-    m_pushService = new bb::network::PushService(QString(), "com.bigboss.bomogramHeadless", this);
+    m_pushService = new bb::network::PushService(QString("5242-68a5060D78R34O443rr71i92cc3107aa556"), "com.bigboss.bomogramHeadless", this);
 
     QObject::connect(m_pushService, SIGNAL(createSessionCompleted(const bb::network::PushStatus&)),
                         this, SLOT(onCreateSessionCompleted(const bb::network::PushStatus&)));
@@ -18,7 +20,7 @@ HeadlessApplication::HeadlessApplication(bb::Application *app) :
     QObject::connect(m_pushService, SIGNAL(createChannelCompleted(const bb::network::PushStatus&, const QString)),
                             this, SLOT(onCreateChannelCompleted(const bb::network::PushStatus&, const QString)));
 
-    //m_pushService->createSession();
+    m_pushService->createSession();
     //m_pushService->createChannel(QUrl());
 
     qDebug() << "HeadlessApplication::HeadlessApplication" << endl;
@@ -33,22 +35,30 @@ void HeadlessApplication::onInvoked(const bb::system::InvokeRequest& request)
     }
     else if(request.action().compare("bb.action.PUSH") == 0)
     {
-        qDebug() << request.data();
+        bb::network::PushPayload payload(request);
+        qDebug() << "PushPayload " << QString(payload.data());
+
+        bb::data::JsonDataAccess jda;
+        QVariantMap rootMap = (jda.loadFromBuffer(payload.data())).toMap();
+        QVariantMap dataMap = rootMap["data"].toMap();
+
     }
 
 }
 
 void HeadlessApplication::onCreateSessionCompleted(const bb::network::PushStatus& status)
 {
+    qDebug() << "HeadlessApplication::onCreateSessionCompleted";
     qDebug() << status.errorDescription() << " ! "<< status.code() << endl;
-    //if (status.code() == PushErrorCode::NoError)
+    if (status.code() == PushErrorCode::NoError)
     {
         //m_pushService->registerToLaunch();
-        m_pushService->createChannel(QUrl());
+        m_pushService->createChannel(QUrl("http://cp5242.pushapi.eval.blackberry.com"));
     }
 }
 
 void HeadlessApplication::onCreateChannelCompleted(const bb::network::PushStatus& status, const QString token)
 {
+    qDebug() << "HeadlessApplication::onCreateChannelCompleted";
     qDebug() << status.errorDescription() << " ! "<< status.code() << "TOKEN " << token << endl;
 }
